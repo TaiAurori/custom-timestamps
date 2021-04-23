76aba236933ef6c45fac006474ac2b9e334888c3
TaiAurori <31465218+TaiAurori@users.noreply.github.com>
2021-04-23 15:11:30 -0400

    add juby to copyright notice because gamer
    shoulda done this a while ago

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 TaiAurori (Gabriel Sylvain) - All Rights Reserved
+/* Copyright (C) 2020 TaiAurori (Gabriel Sylvain), Juby210 - All Rights Reserved
  * You may use, distribute and modify this code under the
  * terms of the MIT license.
  * Basically, you can change and redistribute this code
